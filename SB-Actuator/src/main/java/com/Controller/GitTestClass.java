751c136c3886faec91373cd4a9e39071b2886a1b
swatikakde12 <68200125+swatikakde12@users.noreply.github.com>
2025-04-11 20:52:31 +0530

    Update GitTestClass.java

--- SB-Actuator/src/main/java/com/Controller/GitTestClass.java
+++ SB-Actuator/src/main/java/com/Controller/GitTestClass.java
@@ -7,4 +7,12 @@ public class GitTestClass {
 		System.out.println("this is m1 method");
 		System.out.println("this is m1 method-by dev 2");
 	}
+	public void m4() {
+		System.out.println("this is m4 method");
+		System.out.println("this is m4 method-by dev 2");
+	}
+	public void m5() {
+		System.out.println("this is m5 method");
+		System.out.println("this is m5 method-by dev 2");
+	}
 }
